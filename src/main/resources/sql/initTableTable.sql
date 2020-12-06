5e9b30423c8ede5f0e5c82e01e987090d6a8ceae
chenjun <chenjun8@asiainfo.com>
2020-12-06 16:13:54 +0800

    update

--- src/main/resources/sql/initTableTable.sql
+++ src/main/resources/sql/initTableTable.sql
@@ -1,6 +1,6 @@
 -- sql 表创建脚本--
 
---  上云企业数据模型
+--  企业数据模型
 DROP TABLE IF EXISTS `enterprise`;
 CREATE TABLE `enterprise` (
   `ID` varchar(35) COLLATE utf8mb4_bin NOT NULL COMMENT '主键ID',
@@ -10,11 +10,22 @@ CREATE TABLE `enterprise` (
   `PORDUCT` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '主要产品',
   `DETAIL_TONS` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '所在乡镇',
   `DETAIL_ADDRESS` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '详细地址',
-  `SAFE_CODE` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '安全码：1234（红，橙，黄，绿）',
-  `IS_CLOUD` datetime DEFAULT NULL COMMENT '上云企业',
   `CREATE_DATE` datetime DEFAULT NULL COMMENT '记录首次创建的时间。',
   `UPDATE_DATE` datetime DEFAULT NULL COMMENT '记录每次修改的时间。',
   `REMARK` varchar(2000) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '记录备注。',
   PRIMARY KEY (`ID`),
   KEY `idx_enterprise_name_createtime` (`ENTERPRISE_NAME`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='记录企业国税数据资源数据信息';
+) ENGINE=InnoDB DEFAULT AUTO_INCREMENT=100  CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='企业信息';
+
+DROP TABLE IF EXISTS `cloud_enterprise`;
+CREATE TABLE `cloud_enterprise` (
+  `ID` varchar(35) COLLATE utf8mb4_bin NOT NULL COMMENT '主键ID',
+  `TOWN` varchar(500) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '所在乡镇',
+  `NUM` decimal(18,0) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '数量',
+  `CREATE_DATE` datetime DEFAULT NULL COMMENT '记录首次创建的时间。',
+  `UPDATE_DATE` datetime DEFAULT NULL COMMENT '记录每次修改的时间。',
+  `REMARK` varchar(2000) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '记录备注。',
+  PRIMARY KEY (`ID`),
+  KEY `idx_ID` (`ID`)
+) ENGINE=InnoDB DEFAULT AUTO_INCREMENT=100  CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='企上云数量';
+
