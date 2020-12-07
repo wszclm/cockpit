4d4c1ee58b4400a9307191fd384714d3b35e80db
chenjun <chenjun8@asiainfo.com>
2020-12-07 20:40:26 +0800

    安全生产码，智能预警，安全隐患，核心指标监测 表结构提交

--- src/main/resources/sql/initTableTable.sql
+++ src/main/resources/sql/initTableTable.sql
@@ -29,13 +29,67 @@ CREATE TABLE `cloud_enterprise` (
   KEY `idx_ID` (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COMMENT='企上云数量';
 
-
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'UserID',
-  `name` varchar(32) DEFAULT NULL COMMENT '姓名',
-  `telephone` varchar(16) DEFAULT NULL COMMENT '住宅电话',
-  `username` varchar(255) DEFAULT NULL COMMENT '用户名',
-  `password` varchar(255) DEFAULT NULL COMMENT '密码',
-  `remark` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'UserID',
+  `NAME` varchar(32) DEFAULT NULL COMMENT '姓名',
+  `TELEPHONE` varchar(16) DEFAULT NULL COMMENT '住宅电话',
+  `USERNAME` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `PASSWORD` varchar(255) DEFAULT NULL COMMENT '密码',
+  `REMARK` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=utf8;
+
+-- 安全生产码
+DROP TABLE IF EXISTS `safecode`;
+CREATE TABLE `safecode` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `GREEN` decimal(18,0) DEFAULT NULL COMMENT '绿码数量',
+  `YELLOW` decimal(18,0) DEFAULT NULL COMMENT '黄码数量',
+  `ORANGE` decimal(18,0) DEFAULT NULL COMMENT '橙码数量',
+  `RED` decimal(18,0) DEFAULT NULL COMMENT '红码数量',
+  `CREATE_DATE` datetime DEFAULT NULL COMMENT '记录首次创建的时间。',
+  `UPDATE_DATE` datetime DEFAULT NULL COMMENT '记录每次修改的时间。',
+  `REMARK` varchar(2000) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '记录备注。',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=utf8;
+
+-- 安全隐患
+DROP TABLE IF EXISTS `hidden_danger`;
+CREATE TABLE `hidden_danger` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `ENAME` decimal(18,0) DEFAULT NULL COMMENT '企业名称',
+  `CONTENT` decimal(18,0) DEFAULT NULL COMMENT '隐患内容',
+  `CREATE_DATE` datetime DEFAULT NULL COMMENT '记录首次创建的时间。',
+  `UPDATE_DATE` datetime DEFAULT NULL COMMENT '记录每次修改的时间。',
+  `REMARK` varchar(2000) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '记录备注。',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=utf8;
+
+-- 指标监测
+DROP TABLE IF EXISTS `target_monitor`;
+CREATE TABLE `target_monitor` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `ECOUNT` decimal(18,0) DEFAULT NULL COMMENT '企业总数',
+  `HASCHECK` decimal(18,0) DEFAULT NULL COMMENT '已巡检',
+  `NOTCHECK` decimal(18,0) DEFAULT NULL COMMENT '未巡检',
+  `CREATE_DATE` datetime DEFAULT NULL COMMENT '记录首次创建的时间。',
+  `UPDATE_DATE` datetime DEFAULT NULL COMMENT '记录每次修改的时间。',
+  `REMARK` varchar(2000) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '记录备注。',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=utf8;
+
+-- 智能预警
+DROP TABLE IF EXISTS `intelligent_tips`;
+CREATE TABLE `intelligent_tips` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `ENAME` varchar(200) DEFAULT NULL COMMENT '企业名称',
+  `SDENGER` varchar(1000) DEFAULT NULL COMMENT '安全隐患',
+  `CREATE_DATE` datetime DEFAULT NULL COMMENT '记录首次创建的时间。',
+  `UPDATE_DATE` datetime DEFAULT NULL COMMENT '记录每次修改的时间。',
+  `REMARK` varchar(2000) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '记录备注。',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=utf8;
+
+
+
